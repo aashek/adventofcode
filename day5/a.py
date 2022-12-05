638d065b93e0759af3ca49bb8b1ef8724efad39a
aashek <aashekau@gmail.com>
2022-12-05 20:18:41 +1100

    Day 5 completed (test day)

---
+++ day5/a.py
@@ -0,0 +1,26 @@
+
+instruct = [x.rstrip() for x in open("in.txt").readlines()]
+
+# stacks = [[] for i in range(10)]
+
+# stacks = [list("ZN"), list("MCD"), list("P")]
+
+stacks = [list("NBDTVGZJ"), list("SRMDWPF"), list("VCRSZ"), list("RTJZPHG"), list("TCJNDZQF"), list("NVPWGSFM"), list("GCVBPQ"), list("ZBPN"), list("WPJ")]
+
+for i in instruct:
+    a,b,c,d,e,f = i.split(" ")
+    b = int(b)
+    d = int(d) - 1
+    f = int(f) - 1
+
+    # loop b times
+    # stack 1 = d
+    # stack 2 = f
+    for i in range(b):
+        stacks[f].append(stacks[d].pop())
+
+
+for st in stacks:
+    print(st[-1], end = "")
+
+print()
